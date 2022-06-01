18c0b603757ba7c2c84f381963ceeb87e5087686
Tigi <ventru72@yandex.ru>
2022-06-01 16:47:07 +0500

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson9_tg_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
